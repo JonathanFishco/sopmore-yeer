f5b3fd87559764103bc97b11080ca04e5681ba5d
Jonathan Fishco <jsfis612@learner.dvusd.org>
2022-08-10 09:26:43 -0700

    lets go
    cool

---
+++ C#/goons squad/goons squad/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace goons_squad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   JJJJJJJJJJJ   FFFFFFFFFFF");
+            Console.WriteLine("        J        F          ");
+            Console.WriteLine("        J        F          ");
+            Console.WriteLine("        J        FFFFFF     ");
+            Console.WriteLine("        J        F          ");
+            Console.WriteLine(" J      J        F          ");
+            Console.WriteLine(" J      J        F          ");
+            Console.WriteLine(" JJJJJJJJ        F          ");
+            Console.ReadLine();
+        }
+    }
+}
